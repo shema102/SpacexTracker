9a443b88937995422667a0a8dd55ada7cf884a2e
shema102 <sergeishemshur@gmail.com>
2020-09-11 16:52:45 +0300

    Added mvvm pattern folders

--- app/src/main/java/com/example/spacextracker/view/MainActivity.kt
+++ app/src/main/java/com/example/spacextracker/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.spacextracker
+package com.example.spacextracker.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.spacextracker.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
