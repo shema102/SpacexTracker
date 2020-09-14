d66d8414d6feb605f23f296dd551eda910097de7
shema102 <sergeishemshur@gmail.com>
2020-09-14 16:12:41 +0300

    Added top menu and bottom navigation

--- app/src/main/java/com/example/spacextracker/view/MainActivity.kt
+++ app/src/main/java/com/example/spacextracker/view/MainActivity.kt
@@ -2,11 +2,49 @@ package com.example.spacextracker.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.fragment.app.Fragment
 import com.example.spacextracker.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = bottom_navigation
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, HomeFragment())
+            .commit()
+        bottomNavigation.setOnNavigationItemSelectedListener(bottomNavigationListener)
+    }
+
+    private val bottomNavigationListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            var selectedFragment: Fragment? = null
+
+            when (item.itemId) {
+                R.id.nav_home -> selectedFragment = HomeFragment()
+                R.id.nav_next_launch -> selectedFragment = NextLaunchFragment()
+                R.id.nav_roadster -> selectedFragment = RoadsterFragment()
+            }
+
+            if (selectedFragment != null) {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+
+            return@OnNavigationItemSelectedListener false
+        }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
---
+++ app/src/main/java/com/example/spacextracker/view/RoadsterFragment.kt
@@ -0,0 +1,18 @@
+package com.example.spacextracker.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.spacextracker.R
+
+class RoadsterFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_roadster, container, false)
+    }
+}
