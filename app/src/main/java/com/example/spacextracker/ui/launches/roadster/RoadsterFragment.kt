e0aed7e9ece74edcd32d0eaa6073e6bebd98458c
shema102 <sergeishemshur@gmail.com>
2020-09-19 22:11:06 +0300

    Added test code

--- app/src/main/java/com/example/spacextracker/ui/launches/roadster/RoadsterFragment.kt
+++ app/src/main/java/com/example/spacextracker/ui/launches/roadster/RoadsterFragment.kt
@@ -6,7 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.example.spacextracker.R
+import com.example.spacextracker.data.network.ConnectivityInterceptorImpl
+import com.example.spacextracker.data.network.SpacexApiService
+import com.example.spacextracker.data.network.SpacexNetworkDataSourceImpl
+import kotlinx.android.synthetic.main.roadster_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class RoadsterFragment : Fragment() {
 
@@ -27,6 +35,16 @@ class RoadsterFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(RoadsterViewModel::class.java)
         // TODO: Use the ViewModel
+
+
+        val apiService = SpacexApiService(ConnectivityInterceptorImpl(this.requireContext()))
+        val spacexNetworkDataSource = SpacexNetworkDataSourceImpl(apiService)
+
+        spacexNetworkDataSource.downloadedRoadsterEntry.observe(viewLifecycleOwner, Observer { roadster_text.text = it.toString() })
+
+        GlobalScope.launch(Dispatchers.Main) {
+            spacexNetworkDataSource.fetchRoadster()
+        }
     }
 
 }
